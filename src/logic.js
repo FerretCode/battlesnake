f4ce9a7d73c8e0e569caced3bf7450ac430ff9ba
FerretCode <674313@salemkeizer.org>
2022-03-18 20:05:55 +0000

    refactor defaults for preference

--- src/logic.js
+++ src/logic.js
@@ -1,73 +1,78 @@
-function info() {
-    console.log("INFO")
-    const response = {
-        apiversion: "1",
-        author: "",
-        color: "#888888",
-        head: "default",
-        tail: "default"
-    }
-    return response
-}
+const info = () => {
+  console.log("INFO");
 
-function start(gameState) {
-    console.log(`${gameState.game.id} START`)
-}
+  const response = {
+    apiversion: "1",
+    author: "",
+    color: "#888888",
+    head: "default",
+    tail: "default",
+  };
 
-function end(gameState) {
-    console.log(`${gameState.game.id} END\n`)
-}
+  return response;
+};
 
-function move(gameState) {
-    let possibleMoves = {
-        up: true,
-        down: true,
-        left: true,
-        right: true
-    }
+const start = (gameState) => {
+  console.log(`${gameState.game.id} START`);
+};
 
-    // Step 0: Don't let your Battlesnake move back on its own neck
-    const myHead = gameState.you.head
-    const myNeck = gameState.you.body[1]
-    if (myNeck.x < myHead.x) {
-        possibleMoves.left = false
-    } else if (myNeck.x > myHead.x) {
-        possibleMoves.right = false
-    } else if (myNeck.y < myHead.y) {
-        possibleMoves.down = false
-    } else if (myNeck.y > myHead.y) {
-        possibleMoves.up = false
-    }
+const end = (gameState) => {
+  console.log(`${gameState.game.id} END\n`);
+};
 
-    // TODO: Step 1 - Don't hit walls.
-    // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-    // const boardWidth = gameState.board.width
-    // const boardHeight = gameState.board.height
+const move = (gameState) => {
+  let possibleMoves = {
+    up: true,
+    down: true,
+    left: true,
+    right: true,
+  };
 
-    // TODO: Step 2 - Don't hit yourself.
-    // Use information in gameState to prevent your Battlesnake from colliding with itself.
-    // const mybody = gameState.you.body
+  const myHead = gameState.you.head;
+  const myNeck = gameState.you.body[1];
 
-    // TODO: Step 3 - Don't collide with others.
-    // Use information in gameState to prevent your Battlesnake from colliding with others.
+  if (myNeck.x < myHead.x) {
+    possibleMoves.left = false;
+  } else if (myNeck.x > myHead.x) {
+    possibleMoves.right = false;
+  } else if (myNeck.y < myHead.y) {
+    possibleMoves.down = false;
+  } else if (myNeck.y > myHead.y) {
+    possibleMoves.up = false;
+  }
 
-    // TODO: Step 4 - Find food.
-    // Use information in gameState to seek out and find food.
+  // TODO: Step 1 - Don't hit walls.
+  // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
+  // const boardWidth = gameState.board.width
+  // const boardHeight = gameState.board.height
 
-    // Finally, choose a move from the available safe moves.
-    // TODO: Step 5 - Select a move to make based on strategy, rather than random.
-    const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key])
-    const response = {
-        move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
-    }
+  // TODO: Step 2 - Don't hit yourself.
+  // Use information in gameState to prevent your Battlesnake from colliding with itself.
+  // const mybody = gameState.you.body
 
-    console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`)
-    return response
-}
+  // TODO: Step 3 - Don't collide with others.
+  // Use information in gameState to prevent your Battlesnake from colliding with others.
+
+  // TODO: Step 4 - Find food.
+  // Use information in gameState to seek out and find food.
+
+  // Finally, choose a move from the available safe moves.
+  // TODO: Step 5 - Select a move to make based on strategy, rather than random.
+  const safeMoves = Object.keys(possibleMoves).filter(
+    (key) => possibleMoves[key]
+  );
+  const response = {
+    move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
+  };
+
+  console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`);
+
+  return response;
+};
 
 module.exports = {
-    info: info,
-    start: start,
-    move: move,
-    end: end
-}
+  info: info,
+  start: start,
+  move: move,
+  end: end,
+};
